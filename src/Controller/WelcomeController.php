32c7ee9911eee91e359c8ac8e9241ca9c6190e0e
wael ali <waelali595@gmail.com>
2018-06-04 00:32:10 +0200

    welcome controller

---
+++ src/Controller/WelcomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class WelcomeController extends Controller
+{
+    /**
+     * @Route("/welcome", name="welcome")
+     */
+    public function index()
+    {
+        return $this->render('welcome/index.html.twig', [
+            'controller_name' => 'WelcomeController',
+        ]);
+
+        // return $this->render(view: 'welcome/index.html.twig');
+    }
+}
