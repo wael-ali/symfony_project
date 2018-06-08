9abe0ddd23687050c3a86e978d42fe4c51e1a09b
wael ali <waelali595@gmail.com>
2018-06-08 02:02:33 +0200

    making the contact controller know and deal the form requests

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -5,18 +5,26 @@ namespace App\Controller;
 use App\Form\ContactType;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class ContactController extends Controller
 {
     /**
      * @Route("/contact", name="contact")
      */
-    public function contact()
+    public function contact(Request $request)
     {
         // return $this->render('contact/index.html.twig', [
         //     'controller_name' => 'ContactController',
         // ]);
       $form = $this->createForm(ContactType::class);
+      $form->handleRequest($request);
+
+      if ($form->isSubmitted() && $form->isValid()) {
+        $contactFormData = $form->getData();
+        dump($contactFormData);
+        // do something here ---
+      }
 
       return $this->render('contact/contact.html.twig', [
           'our_form' => $form->createView(),
